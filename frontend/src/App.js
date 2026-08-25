0c17b9eb1635cd0669673d5b9d702ee8e0d048ed
OM PRAKASH <og04914@gmail.com>
2026-08-26 02:23:09 +0530

    Create React app & setting up the Router

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import LandingPage from "./pages/landing";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* <Route path='/home' element= /> */}
+          <Route path='/' element= {<LandingPage />}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
