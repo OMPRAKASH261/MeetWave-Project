82cc0890c75fefa44a3f88bace978b3f8658d998
OM PRAKASH <og04914@gmail.com>
2026-08-26 03:38:52 +0530

    Creating Landing Page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import "../App.css"
+import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
   return (
-    <div>
-      LandingPage
+    <div className='landingPageContainer'>
+        <nav>
+          <div className="navHeader">
+            <h2>MeetWave Video Call</h2>
+          </div>
+          <div className='navlist'>
+              <p>Join as Guest</p>
+              <p>Register</p>
+              <div role='button'>
+                <p>Login</p>
+              </div>
+          </div>
+        </nav>
+        <div className="landingMainContainer">
+          <div>
+            <h1><span style={{ color: "#FF9839"}}>Connect</span> with the right people to drive success</h1>
+            <p>Cover a distance by MeetWave Video Call</p>
+            <div role="button">
+               <Link to={"/home"}> Get Started</Link>
+            </div>
+          </div>
+          <div>
+            <img src="/mobile.png" alt="" />
+          </div>
+        </div>
     </div>
   )
 }
